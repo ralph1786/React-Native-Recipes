6b0ec8191467d39993e5c98eb82e3a43db03de33
ralph1786 <ralph1786@gmail.com>
2019-11-14 13:39:11 -0500

    added tab navigation, side drawer navigation

--- navigation/RecipesNavigator.js
+++ navigation/RecipesNavigator.js
@@ -1,9 +1,25 @@
+import React from "react";
 import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
 import CategoriesScreen from "../screens/CategoriesScreen";
 import RecipeCategoryScreen from "../screens/RecipeCategoryScreen";
 import RecipeDetailsScreen from "../screens/RecipeDetailsScreen";
 import { Platform } from "react-native";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import FavoriteRecipesScreen from "../screens/FavoriteRecipesScreen";
+import { FontAwesome } from "@expo/vector-icons";
+import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs";
+import { createDrawerNavigator } from "react-navigation-drawer";
+import FiltersScreen from "../screens/FiltersScreen";
+
+const defaultNavOptions = {
+  defaultNavigationOptions: {
+    headerStyle: {
+      backgroundColor: Platform.OS === "ios" ? "transparent" : "dodgerblue"
+    },
+    headerTintColor: Platform.OS === "ios" ? "dodgerblue" : "white"
+  }
+};
 
 //createStackNavigator returns a component.
 const RecipeNavigator = createStackNavigator(
@@ -16,14 +32,81 @@ const RecipeNavigator = createStackNavigator(
     },
     RecipeDetails: RecipeDetailsScreen
   },
+  defaultNavOptions
+);
+
+const FavoriteNavigator = createStackNavigator(
   {
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: Platform.OS === "ios" ? "transparent" : "dodgerblue"
+    Favorites: {
+      screen: FavoriteRecipesScreen
+    },
+    RecipeDetails: {
+      screen: RecipeDetailsScreen
+    }
+  },
+  defaultNavOptions
+);
+
+const navigationTabConfig = {
+  Recipes: {
+    screen: RecipeNavigator, //this code makes use of a stack
+    navigationOptions: {
+      tabBarIcon: tabInfo => {
+        return <FontAwesome name="book" size={25} color={tabInfo.tintColor} />;
       },
-      headerTintColor: Platform.OS === "ios" ? "dodgerblue" : "white"
+      tabBarColor: "#f5f5f5"
+    }
+  },
+  Favorites: {
+    screen: FavoriteNavigator, //this code makes use of a stack
+    navigationOptions: {
+      tabBarIcon: tabInfo => {
+        return (
+          <FontAwesome name="star-o" size={25} color={tabInfo.tintColor} />
+        );
+      },
+      tabBarColor: "#f5f5f5"
+    }
+  }
+};
+
+const RecipeTabNavigator =
+  Platform.OS === "android"
+    ? createMaterialBottomTabNavigator(navigationTabConfig, {
+        activeColor: "darkorange",
+        inactiveColor: "#fcbc72",
+        shifting: true
+      })
+    : createBottomTabNavigator(navigationTabConfig, {
+        tabBarOptions: {
+          activeTintColor: "darkorange"
+        }
+      });
+
+const FiltersNavigator = createStackNavigator(
+  {
+    Filters: {
+      screen: FiltersScreen
+    }
+  },
+  defaultNavOptions
+);
+
+const MainNavigator = createDrawerNavigator(
+  {
+    FavoriteRecipes: {
+      screen: RecipeTabNavigator,
+      navigationOptions: {
+        drawerLabel: "Recipes"
+      }
+    },
+    Filters: FiltersNavigator
+  },
+  {
+    contentOptions: {
+      activeTintColor: "darkorange"
     }
   }
 );
 
-export default createAppContainer(RecipeNavigator);
+export default createAppContainer(MainNavigator);
