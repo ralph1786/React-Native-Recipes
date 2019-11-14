21939f008ceeab5eea52c37548899ad13b4c96e6
ralph1786 <ralph1786@gmail.com>
2019-11-14 13:38:14 -0500

    added vertical margin

--- components/RecipeItem.js
+++ components/RecipeItem.js
@@ -50,7 +50,8 @@ const styles = StyleSheet.create({
     width: "100%",
     backgroundColor: "#f5f5f5",
     borderRadius: 10,
-    overflow: "hidden"
+    overflow: "hidden",
+    marginVertical: 10
   },
   recipeHeader: {
     height: "85%"
