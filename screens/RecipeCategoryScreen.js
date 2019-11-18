73e5b1b5df25f1441a6f3bab47031bb2ccc6489c
ralph1786 <ralph1786@gmail.com>
2019-11-18 10:50:40 -0500

    moved content to RecipeList component

--- screens/RecipeCategoryScreen.js
+++ screens/RecipeCategoryScreen.js
@@ -1,39 +1,15 @@
 import React from "react";
-import { View, Text, StyleSheet, FlatList } from "react-native";
 import { CATEGORIES, RECIPES } from "../data/dummy-data";
-import RecipeItem from "../components/RecipeItem";
+import RecipeList from "../components/RecipeList";
 
 const RecipeCategoryScreen = props => {
   const catId = props.navigation.getParam("categoryId");
-
-  const renderRecipeItem = itemData => {
-    return (
-      <RecipeItem
-        recipeData={itemData.item}
-        selectedRecipe={() => {
-          props.navigation.navigate({
-            routeName: "RecipeDetails",
-            params: {
-              chosenRecipeObj: itemData.item
-            }
-          });
-        }}
-      />
-    );
-  };
 
   const displayedRecipes = RECIPES.filter(
     recipe => recipe.categoryIds.indexOf(catId) >= 0
   );
   return (
-    <View style={styles.screen}>
-      <FlatList
-        data={displayedRecipes}
-        keyExtractor={item => item.id}
-        renderItem={renderRecipeItem}
-        style={{ width: "100%" }}
-      />
-    </View>
+    <RecipeList listData={displayedRecipes} navigation={props.navigation} />
   );
 };
 
@@ -46,13 +22,5 @@ RecipeCategoryScreen.navigationOptions = navigationData => {
     headerTitle: selectedCategory.title
   };
 };
-
-const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-  }
-});
 
 export default RecipeCategoryScreen;
