c1000e49e298dfcd92107d5eef0d90c3e7d33144
ralph1786 <ralph1786@gmail.com>
2019-11-19 16:48:54 -0500

    added redux provider

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
 import RecipeNavigator from "./navigation/RecipesNavigator";
+import store from "./store/storeConfig";
+import { Provider } from "react-redux";
 // import { enableScreens } from "react-native-screens";
 
 //Function below uses native screens for better performance.
 // enableScreens();
 
 export default function App() {
-  return <RecipeNavigator />;
+  return (
+    <Provider store={store}>
+      <RecipeNavigator />
+    </Provider>
+  );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center"
+//   }
+// });
