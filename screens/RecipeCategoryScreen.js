713e6a592dbb5dedc884c51e1e5bf9d8787fb509
ralph1786 <ralph1786@gmail.com>
2019-11-21 10:32:55 -0500

    added message if no recipe is found

--- screens/RecipeCategoryScreen.js
+++ screens/RecipeCategoryScreen.js
@@ -1,13 +1,30 @@
 import React from "react";
-import { CATEGORIES, RECIPES } from "../data/dummy-data";
+import { CATEGORIES } from "../data/dummy-data";
 import RecipeList from "../components/RecipeList";
+import { useSelector } from "react-redux";
+import { View, Text, StyleSheet } from "react-native";
 
 const RecipeCategoryScreen = props => {
+  const listOfFilteredRecipes = useSelector(
+    state => state.recipes.filteredRecipes
+  );
+
   const catId = props.navigation.getParam("categoryId");
 
-  const displayedRecipes = RECIPES.filter(
+  const displayedRecipes = listOfFilteredRecipes.filter(
     recipe => recipe.categoryIds.indexOf(catId) >= 0
   );
+
+  if (displayedRecipes.length === 0) {
+    return (
+      <View style={styles.message}>
+        <Text style={styles.text}>
+          No recipes found. Please check your filters!
+        </Text>
+      </View>
+    );
+  }
+
   return (
     <RecipeList listData={displayedRecipes} navigation={props.navigation} />
   );
@@ -22,5 +39,16 @@ RecipeCategoryScreen.navigationOptions = navigationData => {
     headerTitle: selectedCategory.title
   };
 };
+
+const styles = StyleSheet.create({
+  message: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  text: {
+    fontSize: 18
+  }
+});
 
 export default RecipeCategoryScreen;
