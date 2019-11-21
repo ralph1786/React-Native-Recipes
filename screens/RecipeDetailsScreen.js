d11b1b378a4d642f5abf3e9b33b150ab9fb66151
ralph1786 <ralph1786@gmail.com>
2019-11-21 10:34:37 -0500

    added recipe info, styling and connected to redux

--- screens/RecipeDetailsScreen.js
+++ screens/RecipeDetailsScreen.js
@@ -1,32 +1,92 @@
-import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import React, { useEffect, useCallback } from "react";
+import { View, Text, StyleSheet, ScrollView, Image } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import CustomHeaderButton from "../components/CustomHeaderButton";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleFavorite } from "../store/recipeActions";
 
-const RecipeDetailsScreen = props => {
-  const recipeObj = props.navigation.getParam("chosenRecipeObj");
-  console.log(recipeObj);
+const ListItem = props => {
   return (
-    <View style={styles.screen}>
-      <Text>Recipe Details Screen</Text>
-      <Button
-        title="Back To Categories"
-        onPress={() => props.navigation.popToTop()}
-      />
+    <View style={styles.listItem}>
+      <Text>{props.children}</Text>
     </View>
   );
 };
 
+const RecipeDetailsScreen = props => {
+  const availableRecipes = useSelector(state => state.recipes.recipes);
+  const recipeObj = props.navigation.getParam("chosenRecipeObj");
+  const dispatch = useDispatch();
+
+  const toggleFavoriteHandler = useCallback(() => {
+    dispatch(toggleFavorite(recipeObj));
+  }, [dispatch, recipeObj]);
+
+  useEffect(() => {
+    //Use setParams to use a function on the navigation.
+    props.navigation.setParams({
+      toggleFav: toggleFavoriteHandler
+    });
+  }, [toggleFavoriteHandler]);
+
+  const chosenRecipeObj = availableRecipes.find(
+    recipe => recipe.id === recipeObj.id
+  );
+  // console.log(recipeObj);
+  return (
+    <ScrollView>
+      <Image style={styles.image} source={{ uri: chosenRecipeObj.imageUrl }} />
+      <View style={styles.details}>
+        <Text>{chosenRecipeObj.duration}</Text>
+        <Text>{chosenRecipeObj.complexity.toUpperCase()}</Text>
+        <Text>{chosenRecipeObj.affordability.toUpperCase()}</Text>
+      </View>
+      <Text style={styles.title}>Ingredients</Text>
+      {chosenRecipeObj.ingredients.map((ing, index) => (
+        <ListItem key={index}>{ing}</ListItem>
+      ))}
+      <Text style={styles.title}>Instructions</Text>
+      {chosenRecipeObj.instructions.map((instruction, index) => (
+        <ListItem key={index}>{instruction}</ListItem>
+      ))}
+    </ScrollView>
+  );
+};
+
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
+  image: {
+    width: "100%",
+    height: 200
+  },
+  details: {
+    flexDirection: "row",
+    padding: 15,
+    justifyContent: "space-evenly"
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 22,
+    textAlign: "center"
+  },
+  listItem: {
+    marginHorizontal: 20,
+    marginVertical: 10,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    padding: 10
   }
 });
 
 RecipeDetailsScreen.navigationOptions = navigationData => {
-  const recipeObj = navigationData.navigation.getParam("recipeObj");
+  const recipeObj = navigationData.navigation.getParam("chosenRecipeObj");
+  const toggleFavorite = navigationData.navigation.getParam("toggleFav");
   return {
-    headerTitle: recipeObj.title
+    headerTitle: recipeObj.title,
+    headerRight: (
+      <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+        <Item title="Favorite" iconName="star-o" onPress={toggleFavorite} />
+      </HeaderButtons>
+    )
   };
 };
 
